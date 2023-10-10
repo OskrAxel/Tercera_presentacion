72b22c758a43393709a045cadfb165603530e527
unknown <oskr3qsi@gmail.com>
2023-10-10 00:02:14 -0400

    crud crear

--- src/Pages/Admin/Crud.js
+++ src/Pages/Admin/Crud.js
@@ -4,6 +4,11 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Table, Button } from "reactstrap";
 import "../Bec/bec.scss";
+
+import { Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
+
+import { useNavigate } from "react-router-dom";
+
 export default function ListUser() {
   const [users, setUsers] = useState([]);
   useEffect(() => {
@@ -25,6 +30,33 @@ export default function ListUser() {
         getUsers();
       });
   };
+  const navigate = useNavigate();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    axios
+      .post("http://localhost:80/api/user/save", inputs)
+      .then(function (response) {
+        console.log(response.data);
+        abrirCerrarModalInsertar();
+        getUsers();
+        navigate("/");
+      });
+  };
+
+  const abrirCerrarModalInsertar = () => {
+    setModalInsertar(!modalInsertar);
+  };
+  const [modalInsertar, setModalInsertar] = useState(false);
+
+  const [inputs, setInputs] = useState([]);
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
   return (
     <div id="main_content">
       <div className="tra">
@@ -32,11 +64,14 @@ export default function ListUser() {
           <h2 className>Listado Usuarios</h2>
         </div>
         <div id="subt">
-          <Link to="user/create">
-            <Button color="success" size="lg">
-              <FaIcons.FaPlus /> Añadir
-            </Button>
-          </Link>
+          {/* <Link to="user/create"> */}
+          <Button
+            color="success"
+            size="lg"
+            onClick={() => abrirCerrarModalInsertar()}>
+            <FaIcons.FaPlus /> Añadir
+          </Button>
+          {/* </Link> */}
         </div>
         <Table responsive="sm" id="tabl">
           <thead>
@@ -73,6 +108,69 @@ export default function ListUser() {
             ))}
           </tbody>
         </Table>
+
+        <Modal isOpen={modalInsertar}>
+          <ModalHeader>Crear Usuario</ModalHeader>
+          <ModalBody>
+            <div className="form-group">
+              <label>Nombres: </label>
+              <br />
+              <input
+                type="text"
+                className="form-control"
+                name="nombre"
+                onChange={handleChange}
+              />
+              <br />
+              <label>Apellidos: </label>
+              <br />
+              <input
+                type="text"
+                className="form-control"
+                name="apellido"
+                onChange={handleChange}
+              />
+              <br />
+              <label>Correo: </label>
+              <br />
+              <input
+                type="text"
+                className="form-control"
+                name="email"
+                onChange={handleChange}
+              />
+              <br />
+              <label>Clave: </label>
+              <br />
+              <input
+                type="text"
+                className="form-control"
+                name="password"
+                onChange={handleChange}
+              />
+              <br />
+              <label>Celular: </label>
+              <br />
+              <input
+                type="text"
+                className="form-control"
+                name="celular"
+                onChange={handleChange}
+              />
+              <br />
+            </div>
+          </ModalBody>
+          <ModalFooter>
+            <button className="btn btn-primary" onClick={handleSubmit}>
+              Guardar
+            </button>
+            <button
+              className="btn btn-danger"
+              onClick={() => abrirCerrarModalInsertar()}>
+              Cancelar
+            </button>
+          </ModalFooter>
+        </Modal>
       </div>
     </div>
   );
